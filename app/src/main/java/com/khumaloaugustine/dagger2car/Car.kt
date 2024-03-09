ebce59724036a4837a6f38354aef33f55d87f9f2
augustinekhumalo <mr.a.s.khumalo@gmail.com>
2024-03-09 10:52:08 +0200

    Added car component

--- app/src/main/java/com/khumaloaugustine/dagger2car/Car.kt
+++ app/src/main/java/com/khumaloaugustine/dagger2car/Car.kt
@@ -1,7 +1,13 @@
 package com.khumaloaugustine.dagger2car
 
+import android.util.Log
+
 class Car(val wheels: Wheels, val engine: Engine) {
     companion object{
         private const val TAG = "DaggerCar"
+    }
+
+    fun drive(){
+        Log.d(TAG, "I am driving")
     }
 }
